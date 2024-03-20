9dedb3cd5775b4e31330fd809327614224d795b2
midesofek <midesofek@gmail.com>
2024-03-20 18:08:44 +0100

    feat: implemented reverse geocoding to display location in address format

--- components/IncomingStreams.jsx
+++ components/IncomingStreams.jsx
@@ -1,10 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+async function convertCoordToAddress(coords) {
+  const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
+
+  const res = await fetch(
+    `${BASE_URL}?latitude=${coords[0]}&longitude=${coords[1]}`
+  );
+  const data = await res.json();
+
+  const { locality, city, countryCode, continent } = data;
+
+  let cityNameData;
+
+  if (!countryCode && !city && !continent) {
+    cityNameData = "Unidentified Location!";
+  } else {
+    cityNameData = { locality, city, countryCode, continent };
+  }
+
+  return cityNameData;
+}
 
 export const IncomingStreams = function ({ messages }) {
   const [isClosed, setIsClosed] = useState(false);
 
   const toggleMessageWindow = () => {
-    console.log(isClosed);
     setIsClosed((prevState) => !prevState);
   };
 
@@ -24,7 +44,6 @@ export const IncomingStreams = function ({ messages }) {
       </div>
       <div className={`${isClosed ? "" : "message-content"}`}>
         {messages.map((msg) => {
-          console.log("This is: ", msg);
           return <Message msg={msg} />;
         })}
       </div>
@@ -33,9 +52,32 @@ export const IncomingStreams = function ({ messages }) {
 };
 
 function Message({ msg }) {
+  const { data, time } = msg;
+  const [cityData, setCityData] = useState("");
+
+  useEffect(() => {
+    async function convertCityData() {
+      const cityRawData = await convertCoordToAddress(data);
+      console.log(cityRawData);
+      setCityData(cityRawData);
+    }
+    convertCityData();
+  }, []);
+
+  let msgText;
+
+  const formattedTime = new Date(time).toLocaleString();
+
+  if (cityData === "Unidentified Location!") {
+    msgText = "Unidentified Location!";
+  } else {
+    const { locality, city, countryCode, continent } = cityData;
+    msgText = `${locality}, ${city}, ${countryCode}, ${continent} at: ${formattedTime}`;
+  }
+  console.log("This is msgText: ", msgText);
   return (
     <div className="message-content">
-      <p key={msg}>{msg}</p>
+      <p key={msg}>{msgText}</p>
     </div>
   );
 }
